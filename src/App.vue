17c6ffe716a0fc3223ca298f2b8997d1d4e70aef
OSA <osandriiets@gmail.com>
2017-11-16 23:23:52 +0200

    end lections

--- src/App.vue
+++ src/App.vue
@@ -96,17 +96,24 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted"
+                    >Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -123,7 +130,7 @@
                         </ul>
                         <p>Gender: {{ gender }}</p>
                         <p>Priority: {{ selectPriority }}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -132,6 +139,7 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue'
     export default {
         data: function () {
             return{
@@ -144,8 +152,19 @@
                 sendMail: [],
                 selectPriority: "Height",
                 priorities: ["Height", "Meddle", "Low"],
-                gender: "Male"
+                gender: "Male",
+                dataSwitch: true,
+                isSubmitted: false
             }
+        },
+        methods: {
+          submitted(){
+              this.isSubmitted = true;
+              console.log('yyy');
+          }
+        },
+        components: {
+            appSwitch: Switch
         }
     }
 </script>
